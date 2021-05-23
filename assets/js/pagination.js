e9179ba3f2226e66d608c44510ea332b6b49cce1
Juscelino <juscelinodjj@gmail.com>
2021-05-23 17:58:29 -0300

    Improvements in navigation, adding feature go to a specific page

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -4,13 +4,30 @@ const wrapperPagination = document.querySelector('.wrapper-pagination');
 
 const resetPagination = () => wrapperPagination.innerHTML = '';
 
+const getMarkupOption = function () {
+  let loop = numberOfPages;
+  let index = 1;
+  let markup = '';
+  while (loop) {
+    markup += index === currentPage
+      ? `<option value="${index}" selected>${index} - ${numberOfPages}</option>`
+      : `<option value="${index}">${index} - ${numberOfPages}</option>`;
+    index ++;
+    loop --;
+  }
+  return markup;
+};
+
 const getMarkupPagination = function () {
   if (books.length <= amountBookPerPage) {
     return '';
   }
+  const markupOption = getMarkupOption();
   if (currentPage === 1) {
     return `
-      <span class="span-pagination">${currentPage} - ${numberOfPages}</span>
+      <select class="select-pagination" title="Página específica">
+        ${markupOption}
+      </select>
       <a class="button-next-page" title="Página seguinte">
         <i class="icon-angle-double-right"></i>
       </a>
@@ -21,18 +38,29 @@ const getMarkupPagination = function () {
       <a class="button-previus-page" title="Página anterior">
         <i class="icon-angle-double-left"></i>
       </a>
-      <span class="span-pagination">${currentPage} - ${numberOfPages}</span>
+      <select class="select-pagination" title="Página específica">
+        ${markupOption}
+      </select>
     `;
   }
   return `
     <a class="button-previus-page" title="Página anterior">
       <i class="icon-angle-double-left"></i>
     </a>
-    <span class="span-pagination">${currentPage} - ${numberOfPages}</span>
+    <select class="select-pagination" title="Página específica">
+      ${markupOption}
+    </select>
     <a class="button-next-page" title="Página seguinte">
       <i class="icon-angle-double-right"></i>
     </a>
   `;
+};
+
+const navigationTo = function (event) {
+  const selectPagination = event.target;
+  currentPage = Number(selectPagination.value);
+  scrollToTop();
+  setGrid(books);
 };
 
 const navigation = function (event) {
@@ -48,9 +76,13 @@ const enablePagination = function () {
   if (markup) {
     wrapperPagination.innerHTML = markup;
     const buttonPreviousPage = document.querySelector('.button-previus-page');
+    const selectPagination = document.querySelector('.select-pagination');
     const buttonNextPage  = document.querySelector('.button-next-page');
     if (buttonPreviousPage) {
       buttonPreviousPage.addEventListener('click', navigation);
+    }
+    if (selectPagination) {
+      selectPagination.addEventListener('change', navigationTo);
     }
     if (buttonNextPage) {
       buttonNextPage.addEventListener('click', navigation);
